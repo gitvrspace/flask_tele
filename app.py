faa21373c9d80364c8fa31d7f2c5dc3e74cd9aef
gitvrspace <miruarom@gmail.com>
2024-04-08 21:34:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
-
+from flask import Flask,request
+import json
+import urllib.parse as parse
+import urllib. request as req
 app = Flask(__name__)
-
+def baseRequest (command="getMe"):
+    baseURL = 'https://api.telegran.org/bot7050846499:AAEs_EPADdydBPcggtKpQSpXLCLNebIeG3o'
+    res = req.urlopen(baseURL+command)
+    result = json.loads(res.read())['result']
+    return result
+def sendMessage(chat_id, text):
+    query = parse. urlencode([
+    ('chat_id', chat_id),
+    ('text', text)
+    ]}
+    #print (query)
+    baseRequest('sendMessage?'+query)
+    
 @app.route("/")
-def index():
-    return 'hi!'
+def hello_world(): :
+    return 'hi'
+
+@app.route("/937911760:AMGoaceL_hvdr3viab1dBiofapXHpOLRSHA", methods=[ 'POST', 'GET'])
+def telegran():
+    data = request.get_json()
+    print (data)
+    chat_id = data ['message']['chat']['id']
+    text = data['message']['text']
+    sendMessage(chat_id, text)
+    return json.dumps ({success':True})
