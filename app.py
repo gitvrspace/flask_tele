522b4c8e3ba09f6ea950e82555b08461218397bc
gitvrspace <miruarom@gmail.com>
2024-04-08 21:56:23 +0900

    77

--- app.py
+++ app.py
@@ -12,21 +12,21 @@ def baseRequest (command="getMe"):
     return result
 def sendMessage(chat_id, text):
     query = parse.urlencode([
-    ('chat_id', chat_id),
-    ('text', text)
+      ('chat_id', chat_id),
+      ('text', text)
     ])
     #print (query)
     baseRequest('sendMessage?'+query)
     
-@app.route("/")
+@app.route('/')
 def hello_world():
     return 'hi~~'
 
-@app.route('/7050846499:AAEs_EPADdydBPcggtKpQSpXLCLNebIeG3o', methods=[ 'POST', 'GET'])
+@app.route('/7050846499:AAEs_EPADdydBPcggtKpQSpXLCLNebIeG3o', methods=['POST','GET'])
 def telegram():
     data = request.get_json()
     #print (data)
-    chat_id = data ['message']['chat']['id']
+    chat_id = data['message']['chat']['id']
     text = data['message']['text']
     sendMessage(chat_id, text)
     return json.dumps({'success':True})
